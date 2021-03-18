887b96c0fbfea9bdc3c3b2f40a74c58fda25bfbc
alireza8585-code <65468740+alireza8585-code@users.noreply.github.com>
2021-03-18 16:23:57 +0330

    first commit

---
+++ cep.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy
+from random import randrange
+import pyttsx3
+
+eye_cascPath = '../haarcascades/haarcascade_eye_tree_eyeglasses.xml'  #eye detect model
+face_cascPath = '../haarcascades/haarcascade_frontalface_alt.xml'  #face detect model
+
+faceCascade = cv2.CascadeClassifier(face_cascPath)
+eyeCascade = cv2.CascadeClassifier(eye_cascPath)
+
+cap = cv2.VideoCapture(0)
+def eye_open():
+    ret, img = cap.read()
+    if ret:
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect faces in the image
+        faces = faceCascade.detectMultiScale(
+            frame,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            # flags = cv2.CV_HAAR_SCALE_IMAGE
+        )
+        # print("Found {0} faces!".format(len(faces)))
+        if len(faces) > 0:
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            frame_tmp = img[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1, :]
+            frame = frame[faces[0][1]:faces[0][1] + faces[0][3], faces[0][0]:faces[0][0] + faces[0][2]:1]
+            eyes = eyeCascade.detectMultiScale(
+                frame,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                # flags = cv2.CV_HAAR_SCALE_IMAGE
+            )
+            if len(eyes) == 0:
+                return False
+            else:
+                return True
---
+++ testingCEP.py
@@ -0,0 +1,14 @@
+from cep import eye_open
+import screen_brightness_control as sbc
+
+def main():
+    while True:
+        try:
+            if eye_open():
+                sbc.set_brightness(100)
+            else:
+                sbc.set_brightness(0)
+        except sbc.ScreenBrightnessError as error:
+            pass
+if __name__ == '__main__':
+    main()
